9bd49de9d012607b9d887531c334bd1f545b3e11
liujq <liujq@zmcsoft.com>
2019-07-08 19:58:01 +0800

    一次没有灵魂的提交

--- src/main/java/org/bestfeng/template/TemplateApplication.java
+++ src/main/java/org/bestfeng/template/TemplateApplication.java
@@ -1,13 +1,38 @@
 package org.bestfeng.template;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.hswebframework.web.authorization.basic.configuration.EnableAopAuthorize;
+import org.hswebframework.web.authorization.listener.event.AuthorizingHandleBeforeEvent;
+import org.hswebframework.web.dao.Dao;
+import org.hswebframework.web.dev.tools.EnableDevTools;
+import org.hswebframework.web.loggin.aop.EnableAccessLogger;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
 
+/**
+ * @author zhouhao
+ */
 @SpringBootApplication
+@EnableCaching //开启缓存
+@EnableAccessLogger //开启访问日志
+@MapperScan(value = "org.hswebframework.web.demo", markerInterface = Dao.class) //扫描mybatis dao
+@EnableAopAuthorize //启用aop权限控制
+@EnableSwagger2Doc
+@EnableDevTools //开启开发人员工具，生产环境慎用
 public class TemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TemplateApplication.class, args);
     }
 
+    @EventListener
+    public void onApplicationEvent(AuthorizingHandleBeforeEvent event) {
+        //admin 拥有所有权限
+        if (event.getContext().getAuthentication().getUser().getUsername().equals("admin")) {
+            event.setAllow(true);
+        }
+    }
 }
